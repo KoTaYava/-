da7bf69ddfd45f188cb0bd7840ceb57c51cc2dfc
KoTaYava <146347467+KoTaYava@users.noreply.github.com>
2024-02-13 23:06:51 +0300

    Домашняя работа по спринг

--- src/main/java/com/example/Store/Controler/Market.java
+++ src/main/java/com/example/Store/Controler/Market.java
@@ -1,6 +1,9 @@
 package com.example.Store.Controler;
 
+import com.example.Store.Basket;
 import com.example.Store.MarketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,20 +14,21 @@ import java.util.Map;
 @RestController
 @RequestMapping("/order")
 public class Market {
-    private final MarketService marketService;
+    private  MarketService marketService;
 
     public Market(MarketService marketService) {
         this.marketService = marketService;
     }
 
-    @GetMapping("/add")
-    public void basket(@RequestParam(value = "num", required = false) Integer productIdentifier,
-                       @RequestParam(value = "count", required = false) Integer quantity) {
-        marketService.add(productIdentifier, quantity);
-    }
 
     @GetMapping("/get")
-    public Map<Integer, Integer> getBasket() {
-        return marketService.getBasket();
+    public ResponseEntity<Map<Integer,Integer>> getBasket() {
+        return new ResponseEntity<>(marketService.getBasketContents(), HttpStatus.OK);
+
     }
-}
+    @GetMapping("/add")
+    public ResponseEntity<Map<Integer,Integer>> addProduct(@RequestParam int... ids){
+        marketService.addProductToBasket(ids);
+        return new ResponseEntity<>(marketService.getBasketContents(), HttpStatus.OK);
+    }
+    }
